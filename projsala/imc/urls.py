847bc3c9203820ba965861a37e13db0a76059578
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2026-03-27 13:29:46 +0000

    Alteraçõe realizadas em sala de aula.

--- projsala/imc/urls.py
+++ projsala/imc/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('jose',views.jose,name='jose'),
-    path('tabuada/<int:valor>/',views.tabuada,name='tabuada')
+    path('tabuada/<int:valor>/',views.tabuada,name='tabuada'),
+    path('calcular_imc/<int:altura>/<int:peso>/',views.calcular_imc,name='calcular_imc'),
 ]
--- projsala/imc/views.py
+++ projsala/imc/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.​
 def index(request):
-    reposta=HttpResponse("<h1> Bem-vindo(a) à  aplicação  IMC</h1>")
-    return reposta
+    return render(request,'index.html')
 def jose(request):
     return HttpResponse("<h2> Bem-vindo José à  aplicação  IMC</h2>")
 def tabuada(request,valor):
@@ -13,4 +12,10 @@ def tabuada(request,valor):
         html+=f'<tr><td>{valor}X{i+1}</td><td>{valor*(i+1)}</td></tr>'
     html+="</table>"
     resposta=HttpResponse(html)
-    return resposta 
+    return resposta
+
+def calcular_imc(request,altura,peso):
+    altura=altura/100
+    imc=peso/(altura*altura)
+    response=f'Calculo do IMC: {imc}'
+    return HttpResponse(response)
