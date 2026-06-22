e96ab803285ce9a14eb4e110336c7accac932afd
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2026-06-22 14:37:47 +0000

    Adição do context processor.

--- adoteme/principal/utils.py
+++ adoteme/principal/utils.py
@@ -17,3 +17,10 @@ class Utils:
     @staticmethod
     def check_abrigo_group(user):
         return user.groups.filter(name='ABRIGO').exists()
+    
+def adoteme_context(request):
+        context = {
+            'IS_ADOTANTE': Utils.check_adotante(request.user),
+            'IS_ABRIGO': Utils.check_abrigo(request.user),
+        }
+        return context
